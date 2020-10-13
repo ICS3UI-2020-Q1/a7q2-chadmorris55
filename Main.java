b81e7d8313b141e3b96713a06eb5733c01876a70
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-13 09:04:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,23 @@
  */
 public class Main {
 
+
+  //creates method
+  public static void factors(int number){
+    //checks if the result is 0 and if so, prints out whatever "i" is
+    for (int i = 1; i <= number; i++){
+      if (number % i == 0){
+        System.out.println(i);
+      }
+    }
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    //call factors method
+    factors(30);
   }
 }
